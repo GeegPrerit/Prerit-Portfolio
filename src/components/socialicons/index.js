ad571103aeb70071bb0818eb713e1489a2e8e9d3
Prerit Khandelwal <prerit.web@gmail.com>
2024-01-12 16:39:26 +0530

    icon made working

--- src/components/socialicons/index.js
+++ src/components/socialicons/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./style.css";
-// import {
-//   FaGithub,
-//   FaTwitter,
-//   //FaFacebookF,
-//   FaLinkedin,
-//   FaInstagram,
-// } from "react-icons/fa";
+import {
+  FaGithub,
+  FaTwitter,
+  FaFacebookF,
+  FaLinkedin,
+  FaInstagram,
+} from "react-icons/fa";
 import { socialprofils } from "../../content_option";
 
 export const Socialicons = (params) => {
   return (
     <div className="stick_follow_icon">
       <ul>
-        {/* {socialprofils.twitter && (
+        {socialprofils.twitter && (
           <li>
             <a target="_blank" href={socialprofils.twitter}>
               <FaTwitter />
@@ -26,16 +26,16 @@ export const Socialicons = (params) => {
               <FaGithub />
             </a>
           </li>
-        )} */}
-        {/* {socialprofils.facebook && (
+        )}
+        {socialprofils.facebook && (
           <li>
             <a target="_blank" href={socialprofils.facebook}>
               <FaFacebookF />
             </a>
           </li>
-        )} */}
+        )}
 
-        {/* {socialprofils.linkedin && (
+        {socialprofils.linkedin && (
           <li>
             <a target="_blank" href={socialprofils.linkedin}>
               <FaLinkedin />
@@ -48,7 +48,7 @@ export const Socialicons = (params) => {
               <FaInstagram />
             </a>
           </li>
-        )} */}
+        )}
       </ul>
       <p>Follow Me</p>
     </div>
